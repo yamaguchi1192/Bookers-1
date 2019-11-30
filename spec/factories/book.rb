b9f4e8419bf61af2c62248ef126612e0b0c690e1
user <user@usernoMacBook-Air.local>
2019-11-30 17:17:49 +0900

    commit

--- spec/factories/book.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :book do
-    sequence(:title) { |n| "title#{n}"}
-    sequence(:body) { |n| "body#{n}"}
-  end
-end
--- test/controllers/root_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RootControllerTest < ActionDispatch::IntegrationTest
-  test "should get top" do
-    get root_top_url
-    assert_response :success
-  end
-
-end
